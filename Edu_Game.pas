40fbbcbe83847bbfc0a01fd6275c5f6e4f20d1f9
SecrizMRF <secrizkyfadhillah@gmail.com>
2024-12-15 20:52:27 +0700

    Update Edu_Game.pas

--- Edu_Game.pas
+++ Edu_Game.pas
@@ -9,14 +9,14 @@ type
   end;
 var
   datauser : data;
-  words,hints : array[1..30] of string;
+  words,hints : array[1..99] of string;
   word_, wordfind: string;
   counting, SalahTebak: integer;
   tebakan, hint, ulang, masalah, lagi: char;
   i: integer;
   gameOver: boolean;
 
-procedure InisialisasiAset;
+procedure InisialisasiAset; // Subroutine ini mengerjakan inisialisasi data words dan hint
 begin
   clrscr;
   words[1] := 'memory';
@@ -94,10 +94,71 @@ begin
   words[25] := 'christian';
   hints[25] := 'Asisten Lab yang dipanggil Yang Mulia, Baginda, King, etc';
 
-  randomize;
-  word_:=words[random(24)+1];
+  words[26] := 'blockchain';
+  hints[26] := 'Teknologi terdesentralisasi yang digunakan untuk mencatat transaksi dalam bentuk blok yang terhubung secara aman dan transparan.';
 
-  wordfind := StringOfChar('-', Length(word_));
+  words[27] := 'quantum';
+  hints[27] := 'Bidang komputasi yang memanfaatkan prinsip-prinsip fisika kuantum untuk memproses informasi secara eksponensial lebih cepat daripada komputer tradisional.';
+
+  words[28] := 'cryptography';
+  hints[28] := 'Ilmu yang berkaitan dengan teknik untuk menjaga keamanan informasi melalui enkripsi dan dekripsi data.';
+
+  words[29] := 'neural';
+  hints[29] := 'Struktur komputasi yang meniru cara kerja otak manusia untuk memproses informasi, digunakan dalam pembelajaran mesin dan kecerdasan buatan.';
+
+  words[30] := 'compiler';
+  hints[30] := 'Program yang menerjemahkan kode sumber dalam bahasa pemrograman tingkat tinggi menjadi bahasa mesin atau bahasa intermediate.';
+
+  words[31] := 'microservices';
+  hints[31] := 'Arsitektur perangkat lunak yang membagi aplikasi menjadi layanan-layanan kecil yang dapat dikembangkan, di-deploy, dan diskalakan secara terpisah.';
+
+  words[32] := 'docker';
+  hints[32] := 'Platform untuk mengembangkan, mengirimkan, dan menjalankan aplikasi dalam wadah (container) yang terisolasi.';
+
+  words[33] := 'kubernetes';
+  hints[33] := 'Sistem orkestrasi untuk mengelola dan menskalakan aplikasi berbasis container secara otomatis.';
+
+  words[34] := 'heuristic';
+  hints[34] := 'Metode pencarian atau pengambilan keputusan yang menggunakan pendekatan praktis untuk menemukan solusi yang cukup baik untuk masalah yang kompleks.';
+
+  words[35] := 'overfitting';
+  hints[35] := 'Fenomena dalam pembelajaran mesin di mana model terlalu cocok dengan data pelatihan, sehingga kehilangan kemampuan untuk menggeneralisasi pada data baru.';
+
+  words[36] := 'latency';
+  hints[36] := 'Waktu tunda yang terjadi antara pengiriman dan penerimaan data dalam sistem komputer atau jaringan.';
+
+  words[37] := 'concurrency';
+  hints[37] := 'Kemampuan untuk menjalankan beberapa tugas atau proses secara bersamaan dalam suatu sistem komputer.';
+
+  words[38] := 'redundancy';
+  hints[38] := 'Teknik untuk menyalin data atau komponen sistem untuk meningkatkan keandalan dan ketersediaan informasi dalam sistem.';
+
+  words[39] := 'parallelism';
+  hints[39] := 'Teknik di mana beberapa proses dieksekusi secara bersamaan untuk meningkatkan kinerja dan efisiensi dalam komputasi.';
+
+  words[40] := 'turing';
+  hints[40] := 'Seorang ilmuwan komputer yang konsep-konsepnya menjadi dasar bagi teori komputasi dan mesin Turing.';
+
+  words[41] := 'serverless';
+  hints[41] := 'Model komputasi awan yang memungkinkan pengembang menjalankan aplikasi tanpa mengelola server secara langsung, dengan penyedia cloud yang mengelola infrastruktur.';
+
+  words[42] := 'sultithreading';
+  hints[42] := 'Teknik dalam pemrograman yang memungkinkan eksekusi beberapa utas (threads) dalam satu proses untuk meningkatkan efisiensi.';
+
+  words[43] := 'sharding';
+  hints[43] := 'Teknik pembagian data dalam beberapa bagian (shard) untuk meningkatkan skalabilitas dan kinerja sistem database.';
+
+  words[44] := 'branching';
+  hints[44] := 'Teknik yang diimplementasikan pada cara kerja komputer terhadap percabangan perintah atau instruksi.';
+
+  words[45] := 'caching';
+  hints[45] := 'Teknik penyimpanan data sementara dalam lokasi yang lebih cepat diakses untuk meningkatkan kinerja sistem dan mengurangi beban pada sumber daya utama.';
+
+
+  randomize; // Memilih kata secara acak
+  word_:=words[random(45)+1];
+
+  wordfind := StringOfChar('-', Length(word_)); // Mencetak "-" sesuai dengan panjang kata yang telah terpilih
   counting := 0;
   SalahTebak := 0;
   gameOver := false;
@@ -108,7 +169,7 @@ procedure GenerateHint;
 var
   idx: integer;
 begin
-  for idx := 1 to 25 do
+  for idx := 1 to 45 do
   begin
     if words[idx] = word_ then
     begin
@@ -118,87 +179,87 @@ begin
   end;
 end;
 
-procedure Kesempatan;
+procedure Kesempatan; // Menampilkan tampilan setelah user menguinput tebakan nya
 begin
   case SalahTebak of
-    0: begin
-         writeln('  /\__/\     ');
+    0: begin // Tampilan tahap 1
+         writeln('  /\__/\     ',wordfind);
          writeln(' ( ^ w ^) Fufufu ~~~'); 
          writeln('  |    |     '); 
          writeln('  |_||_|     '); 
         end;
  
-    1: begin 
-         writeln('  /\__/\     ');
+    1: begin // Tampilan tahap 2
+         writeln('  /\__/\     ',wordfind);
          writeln(' ( o O o) OH NO!                                                               <{===--*'); 
          writeln('  |    |     '); 
          writeln('  |_||_|     '); 
         end; 
-    2: begin 
-         writeln('  /\__/\     ');
+    2: begin  // Tampilan tahap 3 
+         writeln('  /\__/\     ',wordfind);
          writeln(' ( o O o)                                                         <{===--*'); 
          writeln('  |    |     '); 
          writeln('  |_||_|     '); 
         end; 
-    3: begin 
-         writeln('  /\__/\     ');
+    3: begin  // Tampilan tahap 4 
+         writeln('  /\__/\     ',wordfind);
          writeln(' ( o O o)                                             <{===--*'); 
          writeln('  |    |     '); 
          writeln('  |_||_|     '); 
         end; 
-    4: begin 
-         writeln('  /\__/\     ');
+    4: begin  // Tampilan tahap 5 
+         writeln('  /\__/\     ',wordfind);
          writeln(' ( o O o)                                 <{===--*'); 
          writeln('  |    |     '); 
          writeln('  |_||_|     '); 
         end; 
-    5: begin 
-         writeln('  /\__/\     ');
+    5: begin  // Tampilan tahap 6 
+         writeln('  /\__/\     ',wordfind);
          writeln(' ( o O o)                     <{===--*'); 
          writeln('  |    |     '); 
          writeln('  |_||_|     '); 
         end; 
-    6: begin
-         writeln('  /\__/\     ');
+    6: begin  // Tampilan tahap 7
+         writeln('  /\__/\     ',wordfind);
          writeln(' ( o O o) NOO!!  <{===--*'); 
          writeln('  |    |     '); 
          writeln('  |_||_|     '); 
-         writeln('NOOO! Om Kucing nya kena roket!! >0< ');
+         writeln('NOOO! Om Kucing nya kena roket!! >0< ');   // Losing message
          gameOver := true;
        end;
   end;
 end;
 
-procedure CekTebakan;
+procedure CekTebakan; // Mengecek character tebakan yang diinput user
 var
   found: boolean;
 begin
   found := false;
-  for i := 1 to Length(word_) do
+  for i := 1 to Length(word_) do  // Melakukan pengecekan sesuai dengan panjang kata
   begin
-    if word_[i] = tebakan then
+    if word_[i] = tebakan then // Jika tebakan sesuai
     begin
       wordfind[i] := tebakan;
       found := true;
     end;
   end;
 
-  if not found then
+  if not found then // Jika tebakan salah
   begin
-    SalahTebak := SalahTebak + 1;
-    WriteLn('Tet tot! Kamu punya ', 6 - SalahTebak, ' kesempatan lagi.');
+    SalahTebak := SalahTebak + 1;  // Jika user salah tebak, nilai var ini akan bertambah 1
+    WriteLn('Tet tot! Kamu punya ', 6 - SalahTebak, ' kesempatan lagi.');  // Kesempatan user akan dikurangi dengan jumlah salah tebak
   end
   else
-    WriteLn('Nicee: ', wordfind);
+    WriteLn('Nicee: ', wordfind);  // Jika tebakan huruf user benar, maka huruf yang tertebak akan ditampilkan sesuai dengan urutan huruf penyusun kata.
 end;
 
 begin
   clrscr;
   datauser.times := 0;
-  writeln('//====//====//====//====//===// HALOO! //===//===//===//===//===//===//');
+  writeln('//====//====//====//====//===// HALOO! //===//===//===//===//===//===//'); // Tampilan pembuka dan welcome message
   writeln('=======================================================================');
   with datauser do
-  begin
+  begin  // Inputan data pemain
     write('Nama     : '); readln(nama);
     write('Semester : '); readln(sem);
     write('KOM      : '); readln(kom);
@@ -211,9 +272,10 @@ begin
   writeln;
   masalah := upcase(masalah);
 
-  if masalah = 'Y' then
+  if masalah = 'Y' then  // Jika user ingin belajar dann bermain
   begin
     repeat
+    clrscr;
     WriteLn('       Welcome to Edu Game:       ');
     WriteLn('=== Dodge the Cat from Rocket! ===');
     writeln('            Credit:               ');
@@ -231,7 +293,7 @@ begin
 
     InisialisasiAset;
 
-    while (not gameOver) and (SalahTebak < 6) do
+    while (not gameOver) and (SalahTebak < 6) do   // Looping selama user masih memiliki kesempatan menebak 
     begin
       Write('Enter a letter: ');
       readln(tebakan);
@@ -267,9 +329,9 @@ begin
       CekTebakan;
       Kesempatan;
 
-      if wordfind = word_ then
+      if wordfind = word_ then  // Jika tebakan benar dan sudah selesai
       begin
-        WriteLn('Yeyyy! Om Kucing nya berhasil ngeles!! Kata nya adalah: ', word_);
+        WriteLn('Yeyyy! Om Kucing nya berhasil ngeles!! Kata nya adalah: ', word_); // Winning message
         gameOver := true;
       end;
     end;
@@ -286,10 +348,10 @@ begin
         writeln('Nama     : ',nama);
         writeln('KOM      : ',kom);
         writeln('Semester : ',sem);
-        writeln('Total bermain: ',times);
+        writeln('Total bermain: ',times);  // Menampilkan data pemain dan jumlah pengulangan bermain
       end;
   end
 
-  else 
+  else   // Jika user tidak ingin belajar atau bermain
     write('Baiklah, tetap semangat ya belajarnya!');
 end.
